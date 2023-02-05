c439190ca9423d0c14a487c3018328f680e02358
Phinatech <ugbojudithchinemerem@gmail.com>
2023-02-05 23:34:58 +0100

    Nested relationship

--- controller/bookcontroller.ts
+++ controller/bookcontroller.ts
@@ -2,3 +2,15 @@ import { Request,Response } from "express";
 import authorModel from "../model/authorModel";
 import booksModels from "../model/booksModels";
 
+
+
+const createBooks =async (req:Request,res:Response) => {
+    try {
+        const {tittle,category,summary} = req.body;
+        const myAuthor = await authorModel.findById(req.params.authorBookID);
+    } catch (error) {
+        return res.status(404).json({
+            message:"Couldnt create authors "
+        });
+    }
+};
